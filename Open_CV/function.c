c56992ed27e602fb7eb738defc02de611f1469cb
wangweilai <1689977424@qq.com>
2016-04-09 20:42:40 +0800

    Wangweilai Face_indent test open_cv

---
+++ Open_CV/function.c
@@ -0,0 +1,12 @@
+// http://www.cnblogs.com/mengdd/archive/2012/08/01/2619043.html
+
+CvSeq* cvHaarDetectObjects( 
+const CvArr* image, 
+CvHaarClassifierCascade* cascade, 
+CvMemStorage* storage, 
+double scale_factor CV_DEFAULT(1.1), 
+int min_neighbors CV_DEFAULT(3), 
+int flags CV_DEFAULT(0), 
+CvSize min_size CV_DEFAULT(cvSize(0,0)), 
+CvSize max_size CV_DEFAULT(cvSize(0,0)) 
+);
